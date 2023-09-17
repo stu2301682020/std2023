0f5f43c5a85e5ef520a372dfcb947a137bf52a75
stu2301682020 <stu2301682020@uni-plovdiv.bg>
2023-09-17 14:13:39 +0300

    Add Problem 37_7

---
+++ Problem37_7/Problem37_7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Problem37_7
+{
+    internal class Program
+    {
+        public static bool Expression(int d, int t, int s, int r, bool q, bool v)
+        {
+            return (d == t || d != t && s + d > 5 * r) && !(q || !v) || q && v;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Expression(1, 0, 0, 6, true, false));
+        }
+    }
+}
