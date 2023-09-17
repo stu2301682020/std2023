a2078c9d7d019dfe95b820099bf8aff7c48e59dd
stu2301682020 <stu2301682020@uni-plovdiv.bg>
2023-09-17 07:20:04 +0300

    Add Problem14

---
+++ Problem14/Problem14/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Project
+{
+    internal class Program
+    {
+        public static double CalculateArithmeticProgressionSum(double firstTerm, double commonDifference, int numberOfTerms)
+        {
+            double lastTerm = firstTerm + (numberOfTerms - 1) * commonDifference;
+
+            return (firstTerm + lastTerm) * numberOfTerms / 2;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first term: ");
+            double firstTerm = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the common difference: ");
+            double commonDifference = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of terms: ");
+            int numberOfTerms = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalculateArithmeticProgressionSum(firstTerm, commonDifference, numberOfTerms));
+        }
+    }
+}
