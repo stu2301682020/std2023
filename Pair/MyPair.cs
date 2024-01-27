f838a8d92c8ca3f9e335475c6e62cc08a3493405
fmi <fmi>
2024-01-27 15:10:41 +0200

    Add MyPair

---
+++ Pair/MyPair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class MyPair<T, V>
+{
+
+    public T Key { get; set; }
+    public V Value { get; set; }
+
+    public MyPair(T key, V value)
+    {
+        Key = key;
+        Value = value;
+    }
+
+
+    public override string ToString()
+    {
+        return $"({Key}:{Value})";
+    }
+}
---
+++ Pair/Program.cs
@@ -0,0 +1,9 @@
+﻿var myPair = new MyPair<string, int>("one", 1);
+Console.WriteLine(myPair);
+Console.WriteLine(myPair.Key); 
+Console.WriteLine(myPair.Value);
+
+var mySecondPair = new MyPair<int, int>(4, 400);
+Console.WriteLine(mySecondPair);
+Console.WriteLine(mySecondPair.Key);
+Console.WriteLine(mySecondPair.Value);
